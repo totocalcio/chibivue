b8e1aaacf2e9e3a8ffd4eb9b8b6dc8ce20dc27dc
totocalcio <totohyper@yahoo.co.jp>
2024-04-13 15:44:40 +0900

    project setup

---
+++ examples/playground/vite.config.ts
@@ -0,0 +1,13 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      chibivue: path.resolve(dirname, "../../packages"),
+    },
+  },
+});
